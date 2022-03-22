e6f1477356c31428f284272bcc0de021464b948c
vkotaru <prasanth.kotaru@gmail.com>
2022-03-21 23:44:38 -0700

    PUblish 4 commands

--- qrotor_offboard/scripts/publish_command.py
+++ qrotor_offboard/scripts/publish_command.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 import rospy
-from qrotor_gazebo.msg import Command
+from qrotor_gazebo_plugin.msg import Command
 from geometry_msgs.msg import Vector3
 
+
 def talker():
-    pub = rospy.Publisher('/falcon/command', Command, queue_size=10)
+    pub = []
+    for i in range(4):
+        pub.append(rospy.Publisher('/falcon'+str(i) +
+                   '/command', Command, queue_size=10))
     rospy.init_node('command', anonymous=True)
     rospy.loginfo("Initialzing publish command rosnode")
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(100)  # 10hz
     while not rospy.is_shutdown():
-        msg = Command()
-        msg.mode = Command.MODE_POSITION
-        position = Vector3()
-        position.x, position.y, position.z = 3, 0, 2
-        msg.command.append(position)
-        pub.publish(msg)
+        for i in range(4):
+            msg = Command()
+            msg.mode = Command.MODE_POSITION
+            position = Vector3()
+            if i == 0:
+                position.x, position.y, position.z = 1, 0, 4
+            if i == 1:
+                position.x, position.y, position.z = 0, 1, 4
+            if i == 2:
+                position.x, position.y, position.z = -1, 0, 4
+            if i == 3:
+                position.x, position.y, position.z = 0, -1, 4
+            msg.command.append(position)
+            pub[i].publish(msg)
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
